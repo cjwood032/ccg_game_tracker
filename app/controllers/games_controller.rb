76dbd20d182f520cda0c256672e77b5e0b89d57a
cjwood032 <cjwood032@gmail.com>
2018-08-24 16:01:54 -0400

    create game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,18 @@
 class GamesController < ApplicationController
-
+    before_action :convert, only: [:create]
     def show
     end
     def index
         @game=Game.new
         @decks=current_user.decks
-        #@decks=Deck.all
-        binding.pry
     end
-    def save
+    def create
         @game=Game.new(game_params)
+        @game.deck_id=@deck.id
+        binding.pry
         respond_to do |format|
             if @game.save
+              @game=Game.new
               format.html { redirect_to index, notice: 'Game was successfully recorded.' }
             else
               format.html { render :index, notice: 'Game was not recorded!' }
@@ -20,13 +21,15 @@ class GamesController < ApplicationController
       end
 
     private
+      def convert
+        @deck=Deck.find(params[:game][:deck])
+      end
       def game_params
-        binding.pry
+        #binding.pry
         params.permit(
         :user_id,
         :result,
-        :deck
+        )
         
-      )
       end
 end
