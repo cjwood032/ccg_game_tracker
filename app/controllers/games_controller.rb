ffe02446e9512e4c74180f65e26e5cdc8065bf8b
cjwood032 <cjwood032@gmail.com>
2018-09-08 16:47:10 -0400

    minor improvements

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,7 @@ class GamesController < ApplicationController
     before_action :set_deck, only: [:create_withdeck, :index, :new]
     def show
     end
+    
     def index
         if @deck
           @games=current_user.games.where(deck: @deck).select{|g| g.created_at.today?}
@@ -9,33 +10,32 @@ class GamesController < ApplicationController
           @games=current_user.games.select{|g| g.created_at.today?}
         end
         @last_game=@games.last
-        @games.reverse!
-        #binding.pry
+        @games.reverse!  
     end
-    def new
 
+    def new
       @game=Game.new
-      @decks=current_user.decks
-      #binding.pry
+      @decks=current_user.decks  
     end
+
     def delete
-      #binding.pry
       current_user.games.last.destroy
       respond_to do |format|
         format.html { redirect_to games_path, notice: 'Game was deleted' }
       end
     end
+
     def show
       @game=Game.find(params[:id])
-      #binding.pry
     end
+
     def create_withdeck
       @game=Game.new(game_params)
       @game.deck_id=@deck.id
       @route="/decks/#{:deck_id}/games/new"
-      #binding.pry
       create
     end
+
     def create
       if !@game
         @game=Game.new(game_params)
@@ -43,24 +43,21 @@ class GamesController < ApplicationController
         @deck=Deck.find(id=@game.deck_id)
       end
       @game.user_id=current_user.id
-      make_tags(@game)
-      #binding.pry
+      make_tags(@game)      
       respond_to do |format|
         if @game.save
-          #binding.pry
-          @deck.record_game(@game.result)
-          @game=Game.new
-          #binding.pry
-          @decks=current_user.decks
-          format.html { redirect_to @route, notice: 'Game was successfully recorded.' }
-        else
-          @decks=current_user.decks
-          @message="You need to select a result."
-          #binding.pry
-          format.html { redirect_to @route, notice: 'Game was not recorded, you need to select a result' }
+            @deck.record_game(@game.result)
+            @game=Game.new
+            @decks=current_user.decks
+            format.html { redirect_to @route, notice: 'Game was successfully recorded.' }
+          else
+            @decks=current_user.decks
+            @message="You need to select a result."
+            format.html { redirect_to @route, notice: 'Game was not recorded, you need to select a result' }
         end
       end
     end
+    
     def good
     end
 
@@ -73,7 +70,6 @@ class GamesController < ApplicationController
         @deck=Deck.find_by_id(params[:deck_id])
       end
       def game_params
-        #binding.pry
         params.require(:game).permit(
         :user_id,
         :deck_id,
