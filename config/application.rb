c8c62974d6ecd0768a68714d70adf6123870d3b3
cjwood032 <cjwood032@gmail.com>
2018-09-08 11:05:55 -0400

    fix tags duplication

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-config.session_store :cookie_store, key: '_interslice_session'
-config.middleware.use ActionDispatch::Cookies # Required for all session management
-config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,6 +10,9 @@ module CcgGameTracker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
