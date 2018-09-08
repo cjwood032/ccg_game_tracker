154ea722eaa6d9925ceea872a37dd49bf67e678d
cjwood032 <cjwood032@gmail.com>
2018-09-08 10:54:54 -0400

    omniauth for basic logins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  use OmniAuth::Strategies::Developer
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
   before_action :current_user
