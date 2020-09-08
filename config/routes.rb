a6d49e6812a4cc8dad3a2c671ca0a901766715ae
cjwood032 <cjwood032@gmail.com>
2020-09-08 10:39:36 -0400

    last pages stylized

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   delete "/signout", to: "sessions#destroy"
   #post "/decks/new", to: "decks#new"
   #post "/games/save", to: "games#save"
-  #post "/games/delete", to: "games#delete"
+  post "/games/delete", to: "games#delete"
   post "/decks/:id/delete", to: "decks#destroy"
   post"/users/:id/delete", to: "users#destroy"
-  #get"/users/:id/decks_index", to: "users#decks_index"
+  get"/users/:id/decks_index", to: "users#decks_index"
   get '/auth/google_oauth2/callback' => 'sessions#gcreate'
   #get '/auth/facebook/callback' => 'sessions#gcreate'
   #post '/decks/:deck_id/games', to: "games#create"
