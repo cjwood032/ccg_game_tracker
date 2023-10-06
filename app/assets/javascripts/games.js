c57c0bbde9df0635c8af48ee5f5baf6be95ef4fd
Chris Wood <cjwood032@gmail.com>
2023-10-06 19:45:45 -0400

    formatting updates

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,4 +1,21 @@
-$(document).on("click","#showButton",function(e){
-    e.preventDefault();
-    $('#iform').toggle();
-});
+function showGameOptions() {
+    var modal = document.getElementById("optionalModal");
+    var modalButton = document.getElementById("modalButton");
+    if(modal.style.display != 'block')
+    {
+        modal.style.display = "block";
+        modalButton.setAttribute("style","margin-top:60%");
+        modalButton.setAttribute("textContent","close");
+        this.value="close";
+    }
+    else
+    {
+        modal.style.display = 'none';
+        modalButton.setAttribute("style","margin-top:0")
+        modalButton.setAttribute("text","optional")
+    }
+    
+    var submitButton = document.getElementById("submitGame")
+    //submitButton.setAttribute("style","margin-top:60%")
+    
+};
